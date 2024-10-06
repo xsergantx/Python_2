a95d146e6a0d97dee75472798741b1b33c612aa6
xsergantx <sergey.yurga42@gmail.com>
2024-10-06 19:44:27 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+import fake_math as black
+import true_math as Blue
+
+result1 = black(69, 3)
+result2 = black(3, 0)
+result3 = Blue(49, 7)
+result4 = Blue(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
