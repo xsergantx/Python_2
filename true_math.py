14bb1c8c6e3629079873e509f7929559f9003df9
xsergantx <sergey.yurga42@gmail.com>
2024-10-06 19:43:50 +0700

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,13 @@
+# истиная математика
+def divide(first, second):
+    if first > 0 and second > 0:
+        print (first/second)
+        return (" ")
+    elif second == 0:
+        print("inf")
+        return (" ")
+
+result3 = divide(49, 7)
+result4 = divide(15, 0)
+print(result3)
+print(result4)
