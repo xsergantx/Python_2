d367734fc53cce9b2b792644697e59b4d9ed1292
xsergantx <sergey.yurga42@gmail.com>
2024-10-28 15:04:44 +0700

    Add files via upload

---
+++ module_6_3.py
@@ -0,0 +1,57 @@
+class Horse:# - класс описывающий лошадь
+    """
+    атрибуты лошади
+    """
+    def __init__ (self, *args):
+        self.x_distance = 0 # пройденный путь
+        self.sound = 'Frrr' # - звук, который издаёт лошадь.
+        super().__init__(*args)
+    """
+    метод
+    """
+    def run (self, dx): # - изменение дистанции
+        self.x_distance += dx
+
+
+class Eagle:# - класс описывающий орла
+    """
+    атрибуты орла
+    """
+    def __init__ (self, *args):
+        self.y_distance = 0 # - высота полёта
+        self.sound = 'I train, eat, sleep, and repeat' # - звук, который издаёт орёл(отсылка)
+        super().__init__(*args)
+    """
+    метод
+    """
+    def fly (self, dy):  # - изменение дистанции
+        self.y_distance += dy
+
+
+class Pegasus(Horse,Eagle): #- класс описывающий пегаса
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+    def move(self, dx, dy):
+        self.run(dx)
+        self.fly(dy)
+
+
+    def get_pos(self):
+        return self.x_distance, self.y_distance
+
+
+    def voice(self):
+        print(self.sound)
+
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
