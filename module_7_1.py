6667bf7cb3860b2a120a1d837c720a32f2e114b9
xsergantx <sergey.yurga42@gmail.com>
2024-11-01 22:50:03 +0700

    Add files via upload

---
+++ module_7_1.py
@@ -0,0 +1,64 @@
+""""
+Задача "Учёт товаров":
+Необходимо реализовать 2 класса Product и Shop, с помощью которых будет производиться запись в файл с продуктами.
+Объекты класса Product будут создаваться следующим образом - Product('Potato', 50.0, 'Vagetables') и
+обладать следующими свойствами:
+Атрибут name - название продукта (строка).
+Атрибут weight - общий вес товара (дробное число) (5.4, 52.8 и т.п.).
+Атрибут category - категория товара (строка).
+Метод __str__, который возвращает строку в формате '<название>, <вес>, <категория>'.
+Все данные в строке разделены запятой с пробелами.
+
+Объекты класса Shop будут создаваться следующим образом - Shop() и обладать следующими свойствами:
+Инкапсулированный атрибут __file_name = 'products.txt'.
+Метод get_products(self), который считывает всю информацию из файла __file_name,
+закрывает его и возвращает единую строку со всеми товарами из файла __file_name.
+Метод add(self, *products), который принимает неограниченное количество объектов класса Product.
+Добавляет в файл __file_name каждый продукт из products, если его ещё нет в файле (по названию).
+Если такой продукт уже есть, то не добавляет и выводит строку 'Продукт <название> уже есть в магазине' .
+
+"""
+
+from pprint import pprint
+
+class Product:
+    def __init__ (self, name: str, weight: float, category: str):
+        self.name = name # название
+        self.weight = weight  # вес
+        self.category = category  # категория
+
+    def __str__(self):
+        str_product = f'{self.name}, {self.weight}, {self.category}'
+        return str_product
+
+class Shop:
+    __file_name = 'products.txt' #Инкапсулированный атрибут
+
+    def get_product(self): #который считывает всю информацию из файла
+        file = open(self.__file_name, 'r+')
+        prod_str = file.read()
+        file.close()
+        # pprint(prod_str)
+        return prod_str
+
+    def add (self, *products):# принимает неограниченное количество объектов класса Product
+        __file_name = self.get_product()
+        for i in products:
+            if self.get_product().find(f'{i.name},') == -1:
+                file = open(self.__file_name, 'a')
+                file.write(f'{i}\n')
+                file.close()
+            else:
+                print(f'Продукт {i.name} уже есть в магазине')
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_product())
