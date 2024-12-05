f76bbd0b980457843706a9bb0be167a7427d353e
xsergantx <sergey.yurga42@gmail.com>
2024-12-05 15:00:55 +0700

    Add files via upload

---
+++ module_10_4.py
@@ -0,0 +1,110 @@
+"""
+Цель: Применить очереди в работе с потоками, используя класс Queue.
+
+Задача "Потоки гостей в кафе":
+Необходимо имитировать ситуацию с посещением гостями кафе.
+Создайте 3 класса: Table, Guest и Cafe.
+Класс Table:
+Объекты этого класса должны создаваться следующим способом - Table(1)
+Обладать атрибутами number - номер стола и guest - гость, который сидит за этим столом (по умолчанию None)
+Класс Guest:
+Должен наследоваться от класса Thread (быть потоком).
+Объекты этого класса должны создаваться следующим способом - Guest('Vasya').
+Обладать атрибутом name - имя гостя.
+Обладать методом run, где происходит ожидание случайным образом от 3 до 10 секунд.
+Класс Cafe:
+Объекты этого класса должны создаваться следующим способом - Cafe(Table(1), Table(2),....)
+Обладать атрибутами queue - очередь (объект класса Queue) и tables - столы в этом кафе (любая коллекция).
+Обладать методами guest_arrival (прибытие гостей) и discuss_guests (обслужить гостей).
+Метод guest_arrival(self, *guests):
+Должен принимать неограниченное кол-во гостей (объектов класса Guest).
+Далее, если есть свободный стол, то садить гостя за стол (назначать столу guest), запускать поток гостя и выводить на экран строку "<имя гостя> сел(-а) за стол номер <номер стола>".
+Если же свободных столов для посадки не осталось, то помещать гостя в очередь queue и выводить сообщение "<имя гостя> в очереди".
+Метод discuss_guests(self):
+Этот метод имитирует процесс обслуживания гостей.
+Обслуживание должно происходить пока очередь не пустая (метод empty) или хотя бы один стол занят.
+Если за столом есть гость(поток) и гость(поток) закончил приём пищи(поток завершил работу - метод is_alive), то вывести строки "<имя гостя за текущим столом> покушал(-а) и ушёл(ушла)" и "Стол номер <номер стола> свободен". Так же текущий стол освобождается (table.guest = None).
+Если очередь ещё не пуста (метод empty) и стол один из столов освободился (None), то текущему столу присваивается гость взятый из очереди (queue.get()). Далее выводится строка "<имя гостя из очереди> вышел(-ла) из очереди и сел(-а) за стол номер <номер стола>"
+Далее запустить поток этого гостя (start)
+Таким образом мы получаем 3 класса на основе которых имитируется работа кафе:
+Table - стол, хранит информацию о находящемся за ним гостем (Guest).
+Guest - гость, поток, при запуске которого происходит задержка от 3 до 10 секунд.
+Cafe - кафе, в котором есть определённое кол-во столов и происходит имитация прибытия гостей (guest_arrival) и их обслуживания (discuss_guests).
+"""
+
+from threading import Thread
+from time import sleep
+from random import randint
+from queue import Queue
+
+
+class Table:
+    def __init__(self, number: int):
+        self.number = number  # номер стола
+        self.guest = None  # гость, который сидит за этим столом
+
+
+class Guest(Thread):# наследование потока
+    def __init__(self, name: str):
+        super().__init__()
+        self.name = name  # имя гостя
+
+    def run(self):
+        sleep(randint(3, 10))# ожидание от 3 до 10 сек.
+
+
+class Cafe:# клас кафе
+    def __init__(self, *tables: Table):
+        self.tables = tables  # столы в этом кафе
+        self.queue = Queue()  # очередь
+
+    def is_vacant(self):
+        '''кафе пустое'''
+        return not any(t.guest for t in self.tables)
+
+    def guest_arrival(self, *guests: Guest):#прибытие гостей
+        '''прибытие гостей'''
+        for guest in guests:
+            vacant_table_found = False
+            for table in self.tables:
+                if not table.guest:
+                    table.guest = guest
+                    guest.start()
+                    print(f'{guest.name} сел(-а) за стол номер {table.number}')
+                    vacant_table_found = True
+                    break
+            if not vacant_table_found:
+                self.queue.put(guest)
+                print(f'{guest.name} в очереди')
+
+    def discuss_guests(self):
+        '''обслужить гостей'''
+        while not (self.queue.empty() and self.is_vacant()):
+            for table in self.tables:
+                if not table.guest:
+                    if not self.queue.empty():
+                        table.guest = self.queue.get()
+                        print(f'{table.guest.name} вышел(-ла) из очереди',
+                              f'и сел(-а) за стол номер {table.number}')
+                        table.guest.start()
+                else:
+                    if not table.guest.is_alive():
+                        print(f'{table.guest.name} покушал(-а) и ушёл(ушла)')
+                        print(f'Стол номер {table.number} свободен')
+                        table.guest = None
+
+
+if __name__ == '__main__':
+    # Создание столов
+    tables = [Table(number) for number in range(1, 6)]
+    # Имена гостей
+    guests_names = ['Мария', 'Олег', 'Вадим', 'Сергей', 'Даша', 'Арман',
+                    'Виктория', 'Никита', 'Галина', 'Павел', 'Илья',
+                    'Александра']
+    # Создание гостей
+    guests = [Guest(name) for name in guests_names]
+    # Заполнение кафе столами
+    cafe = Cafe(*tables)
+    # Приём гостей
+    cafe.guest_arrival(*guests)
+    # Обслуживание гостей
